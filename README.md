7cab080b92309a45a8839d2a72fa474ce3dd9635
LINK1733 <zaxios@hotmail.com>
2020-01-26 21:50:14 -0600

    Made program sleep for work duration and crossed off to do 1 on list (#4)

--- README.md
+++ README.md
@@ -5,8 +5,16 @@ A task timer! Cool things to come, maybe.
 ## To Do
 
 ~~1. Make timer that writes to console once time is up.~~
-1. Make break 5 minutes
-1. Repeat
-1. Audible cue for transitions
-1. To-Do list for actions 
-1. GUI - TBD
+
+~~2. Make break 5 minutes~~
+
+~~3. Repeat~~
+
+~~4. Make configurable time lengths~~
+
+~~5. Change time to minutes~~
+
+6. For dev purposes, use seconds
+6. Audible cue for transitions
+7. To-Do list for actions 
+8. GUI - TBD
--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 import time
 
-work_time_duration = 5
+print("-----------------Task Timer-----------------")
+print("Times are in minutes.")
+work_time_duration = int(input("How long do you want to work? ")) * 60
+break_time_duration = int(input("How long do you want the breaks? ")) * 60
 
 
 def start():
     print('Start working!')
     time.sleep(work_time_duration)
     print('Take a break')
+    time.sleep(break_time_duration)
+    print("Break's over")
+
+    start()
+
 
 start()
