c68b3132bcb03d4eb7af03a286e1ad655c3a2806
LINK1733 <zaxios@hotmail.com>
2020-01-26 23:01:56 -0600

    Added prompt for to do list and have it say which item of list you are on when each timer starts. (#7)

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import sys
 from playsound import playsound
 
 use_seconds = 'seconds' in sys.argv
-print(use_seconds)
 
 print("-----------------Task Timer-----------------")
 
@@ -11,6 +10,8 @@ print(f"Times are in {'seconds' if use_seconds else 'minutes'}.")
 
 work_time_duration = float(input("How long do you want to work? "))
 break_time_duration = float(input("How long do you want the breaks? "))
+to_do_list = input("Enter the list of things to do separated by a comma: ")
+to_do_list = to_do_list.split(", ")
 
 if not use_seconds:
     work_time_duration *= 60
@@ -22,15 +23,16 @@ def alarm():
 
 
 def start():
-    print('Start working!')
-    time.sleep(work_time_duration)
-    print('Take a break')
-    alarm()
-    time.sleep(break_time_duration)
-    print("Break's over")
-    alarm()
+    for to_do_list_item in to_do_list:
+        print(f"Time for: ", to_do_list_item)
+        time.sleep(work_time_duration)
+        print('Take a break')
+        alarm()
+        time.sleep(break_time_duration)
+        print("Break's over")
+        alarm()
 
-    start()
+    print("Congratulations! You did it!")
 
 
 start()
